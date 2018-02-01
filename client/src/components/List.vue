7e0a70e718d081257f350f4ceba2ed927448b849
Solange Blundi <solange.blundi@gmail.com>
2018-02-01 13:25:20 -0300

    Cambio los estilos de la lista

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="todo-list">
-        <h1>TO-DO LIST</h1>
+        <div class="todo-list-header">
+          <h1>TO-DO LIST</h1>
+        </div>
         <div v-for="todo in todos" :key="todo.id">
           <div class="card">
             <p>
@@ -36,8 +38,14 @@ export default {
 </script>
 
 <style>
-.card {
+.todo-list {
   border: 2px solid Gray;
   border-radius: 10px;
+  margin: auto;
+  width: 30%;
+}
+
+.todo-list-header {
+  border-bottom: 2px solid Gray;
 }
 </style>
