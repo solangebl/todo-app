2eabc50745fee644508c69d7ee39619f304e45db
sol <solange.blundi@gmail.com>
2018-02-14 00:41:47 -0300

    Agrego template envs para el cliente y agrego .env.js al gitignore

--- client/config/dev.env.template.js
+++ client/config/dev.env.template.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"http://localhost:"'
+  API_ENDPOINT: '"http://localhost:8081"'
 })
